28e5945fcd126300fc9b16a9a731da0be8925387
j253cao <j253cao@uwaterloo.ca>
2024-02-14 10:58:11 -0500

    get both tests to pass

--- src/components/RestaurantList.spec.js
+++ src/components/RestaurantList.spec.js
@@ -1,9 +1,29 @@
-import {render} from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import RestaurantList from './RestaurantList';
 describe('RestaurantList', () => {
   it('loads restaurants on first render', () => {
     const loadRestaurants = jest.fn().mockName('loadRestaurants');
-    render(<RestaurantList loadRestaurants={loadRestaurants} />);
+    const restaurants = [];
+
+    render(
+      <RestaurantList
+        loadRestaurants={loadRestaurants}
+        restaurants={restaurants}
+      />,
+    );
     expect(loadRestaurants).toHaveBeenCalled();
+  });
+
+  it('displays the restaurants', () => {
+    const noop = () => {};
+    const restaurants = [
+      {id: 749, name: 'Salad Place'},
+      {id: 750, name: 'Pasta Place'},
+    ];
+
+    render(<RestaurantList loadRestaurants={noop} restaurants={restaurants} />);
+
+    expect(screen.getByText('Salad Place')).toBeInTheDocument();
+    expect(screen.getByText('Pasta Place')).toBeInTheDocument();
   });
 });
