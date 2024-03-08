ffa369a68f57e2ce54dd3149f3ebb827bd60c36e
j253cao <j253cao@uwaterloo.ca>
2024-03-08 10:57:27 -0500

    March 8 super secret lab bonus complete

---
+++ src/components/AddTodo.js
@@ -0,0 +1,27 @@
+import React, {useState} from 'react';
+import {connect} from 'react-redux';
+import {addTodo} from '../redux/actions';
+
+const AddTodo = ({addTodo}) => {
+  const [input, setInput] = useState('');
+
+  const updateInput = e => {
+    setInput(e.target.value);
+  };
+
+  const handleAddTodo = () => {
+    addTodo(input);
+    setInput('');
+  };
+
+  return (
+    <div>
+      <input onChange={updateInput} value={input} />
+      <button className="add-todo" onClick={handleAddTodo}>
+        Add Todo
+      </button>
+    </div>
+  );
+};
+
+export default connect(null, {addTodo})(AddTodo);
